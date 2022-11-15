2cbd19fcb8485a71ce28b154c4276a9f23a74211
Permyakov Anton <permyakov.a@spectrumdata.ru>
2022-11-15 20:00:37 +0500

    init commit

---
+++ map-handler.js
@@ -0,0 +1,82 @@
+import points from '/points.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const { ymaps } = window;
+  ymaps.ready(init);
+
+  function init() {
+    const nodeMap = document.querySelector('[data-map]');
+    console.log(nodeMap);
+    var myMap = new ymaps.Map(nodeMap, {
+      center: [56.83580743, 60.61689559],
+      zoom: 10,
+    });
+
+    const homePoints = points.filter((point) => point.type === 'home');
+    const homeCluster = getCluster(homePoints, 'home');
+    myMap.geoObjects.add(homeCluster);
+
+    const mountainPoints = points.filter((point) => point.type === 'mountain');
+    const mountainCluster = getCluster(mountainPoints, 'mountain');
+    myMap.geoObjects.add(mountainCluster);
+  }
+
+
+  /**
+   *
+   * @param point []
+   * @param type 'home' | 'mountain'
+   * @return ymaps.Clusterer
+   */
+  function getCluster(point, type) {
+    const cluster = new ymaps.Clusterer({
+      clusterIconLayout: 'default#pieChart'
+    })
+
+    let preset = 'islands#blueDotIcon';
+    if (type === 'home') {
+      preset = 'islands#redHomeIcon';
+    } else if (type === 'mountain') {
+      preset = 'islands#darkOrangeMountainIcon';
+    }
+
+    return point.reduce((clusterer, point) => {
+      let balloonContent = '';
+      if (point.address) {
+        balloonContent += `<h3>${point.address}</h3>`
+      }
+      if (point.link) {
+        balloonContent += `<a href="${point.link}">${point.link}</a>`
+      }
+      if (point.price) {
+        balloonContent += `<p>Цена: ${point.price.toLocaleString()}</p>`;
+      }
+      if (point.options) {
+        balloonContent += '<ul>'
+        Object.entries(point.options).forEach(([key, value]) => {
+          balloonContent += `<li><b>${key}:</b> ${value}</li>`
+        });
+        balloonContent += '</ul>';
+      }
+      if (point.imags) {
+        balloonContent += '<div class="map-imag-slider">'
+        point.imags.forEach((value) => {
+          balloonContent += `<img src="${value}" alt="" class="map-imag-slider__item">`
+        });
+        balloonContent += '</div>';
+      }
+
+
+      const placemark = new ymaps.Placemark(point.cord, {
+        hintContent: 'hintContent',
+        balloonContent: balloonContent,
+      }, {
+        preset,
+      });
+
+      clusterer.add(placemark);
+
+      return clusterer;
+    }, cluster);
+  }
+});
---
+++ points.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    cord: [56.77420921, 60.64602682],
+    imags: [
+      'https://uktus.ural.ski/wp-content/uploads/2022/11/photo_2022-11-08_17-09-40-400x256.jpg',
+    ],
+    type: 'mountain',
+    link: 'https://uktus.ural.ski/',
+    address: 'Зимняя улица, 27, Екатеринбург',
+  },
+  {
+    cord: [56.999333, 60.525789],
+    imags: [
+      'https://vsezagorod.ru/img/user/catalog/19/prostokvashino_9d6c4d0bebfbb1f.JPG',
+      'https://vsezagorod.ru/img/user/catalog/19/prostokvashino_9d6c4d0bebfbb1f.JPG',
+    ],
+    type: 'home',
+    link: 'https://vsezagorod.ru/catalog/prostokvashino.html',
+    address: 'Западная улица, 77, СНТ Мираж, Верхняя Пышма',
+    price: 24000,
+    options: {
+      'Баня': '3 часа включено в стоимость, свыше 500 руб/час',
+      'Купель': '4000 руб на все время проживания',
+    }
+  },
+]
