06a9c701fb32b09af074e293c0839d576b2709fd
LeTraceurSnork <le_traceur_snork@mail.ru>
2022-11-16 23:53:14 +0500

    add some avito ads

--- points.js
+++ points.js
@@ -38,6 +38,7 @@ export default [
     link: 'http://volchiha.ru/',
   },
   {
+    title: 'ВСЕ ЗАГОРОД',
     cord: [56.999333, 60.525789],
     imags: [
       'https://vsezagorod.ru/img/user/catalog/19/prostokvashino_9d6c4d0bebfbb1f.JPG',
@@ -53,6 +54,7 @@ export default [
     }
   },
   {
+    title: 'Коттедж в Ревде',
     cord: [56.78790145231818, 59.90539428959414],
     imags: [],
     type: 'home',
@@ -66,6 +68,7 @@ export default [
     }
   },
   {
+    title: 'Хвоя HOUSE',
     cord: [56.881155, 60.951311],
     imags: [
       '/activity-on-map/images/hvoya-house/photo_2022-11-15_22-51-42.jpg',
@@ -80,6 +83,7 @@ export default [
     options: {}
   },
   {
+    title: 'Место Силы',
     cord: [56.773557, 59.807044],
     imags: [
       'https://family-place.ru/static/uploads/camps/16680062902016125.jpg',
@@ -102,6 +106,77 @@ export default [
       'Мангальная зона': 'есть',
       'Банный чан': 'есть',
       'Количество спален': '3',
+    }
+  },
+  {
+    title: 'Лесное озеро',
+    cord: [55.021897, 59.940655],
+    imags: [
+      '',
+    ],
+    type: 'home',
+    link: 'https://www.avito.ru/miass/doma_dachi_kottedzhi/kottedzh_60m_na_uchastke_100sot._930785931',
+    address: 'На юго-западе от Миасса',
+    price: 70000,
+    options: {
+      'Озеро': 'Прямо под боком',
+      'Баня': 'включено',
+      'Чан': 'доп'
+    }
+  },
+  {
+    title: 'Домик в Первоуральске',
+    cord: [56.910742, 60.008107],
+    imags: [
+      'https://79.img.avito.st/image/1/1.0hinOLayfvGRkbz0jwu9T1qbfvcHnXw.x1rbHlSvNNQqgJuP5Br8roj3CBdOKCXtWJlVGZrj6w8',
+      'https://81.img.avito.st/image/1/1.zBdyxbayYP5EbKL7ZvajQI9mYPjSYGI.JiHxXnXeBvmDtFRQeH80jAt4-D6WD9oDhFztQAIxSHk',
+      'https://39.img.avito.st/image/1/1.OhinSLaylvGR4VT032RVT1rrlvcH7ZQ.oeqP5i1OfPeRch-qI2j1ei1vJyzDl5-rPJTzHN3o8po',
+      'https://74.img.avito.st/image/1/1.15XUkLaye3ziObl59qO4wikze3p0NXk.4Yp28ijHgqz-8mTgTc65urtDrP_H7DY-cnxAdjUwMP0',
+      'https://15.img.avito.st/image/1/1.EgBFsrayvulzG3zsbZ59V7gRvu_lF7w.lNTH7Q641SDQqymLJEDZaFa9-huj4wf5xm0GTJqDl6U',
+      'https://77.img.avito.st/image/1/1.0CfwzLayfM7GZb7L3P-_cA1vfMhQaX4.wLKR-tFAXKF-ZFD7rGEzcs-Wgr6Rz8SlK4BhgpPgAT4',
+      'https://97.img.avito.st/image/1/1.nNNKSrayMDp84_I_fnSClKnpMDzq7zI.Jw4xV_ta1-r5QT_dlaAGOeuZXGATVLuS-gBPWq77JAY',
+      'https://05.img.avito.st/image/1/1.9I1KZ7ayWGR8zpphLlSb2rfEWGLqwlo.GOdvh-yOZTVilmAWNpAfwQ89BWRXJTDWMA7sdERBvFM',
+      'https://36.img.avito.st/image/1/1.OXM5_LaylZoPVVefR9BWJMRflZyZWZc._1X7l6INn7U4Ke9oVFnT8Wz6bMFn6ifiBlb7P3c3NAE',
+      'https://89.img.avito.st/image/1/1.xC_QX7ayaMbm9qrD1GyreC38aMBw-mo.nXMahuMwGC1T03NCLBTWIsujDf8bh5euPTetvU-rQJE',
+      'https://07.img.avito.st/image/1/1.GvpdULaythNr-XQWZXx1raDzthX99bQ.sSgLaNhL6vAZ07PxJrGXTxGG7ymOu1Z-qvjaLDLL6iI',
+      'https://06.img.avito.st/image/1/1.99jIwbayWzH-aJk0qvKYjzViWzdoZFk.dco5tIk3mcKxLmDpVd8QcUmpp7uBAdWopneuPyax-dA',
+      'https://93.img.avito.st/image/1/1.wK3Zb7aybETvxq5B1Vyv-iTMbEJ5ym4.f6OAPMKrJuyciX-7xRb9G2gIYoD5259lpZ6U4lBY4ec',
+      'https://98.img.avito.st/image/1/1._4mF3bayU2CzdJFl9-6Q3nh-U2YleFE.S3hgC1CmDmsCk-TbRZ-9SLkEgwHMpt_2n2PgZvxL4ZU',
+    ],
+    type: 'home',
+    link: 'https://www.avito.ru/pervouralsk/doma_dachi_kottedzhi/kottedzh_190_m_na_uchastke_10_sot._2051799437',
+    address: 'Первоуральск, Цеховая ул. 26Г',
+    price: 70000,
+    options: {
+      'Гора Пильная': 'пешком можно дойти',
+      'Спальных мест': 8
+    }
+  },
+  {
+    title: 'Домик на Белой',
+    cord: [57.656566, 59.677016],
+    imags: [
+      'https://79.img.avito.st/image/1/1.Lqd7ELaygk5NuUBLK3Avt6ezgkjbtYA.6FjbhgDWj4tBgzaw7XhxfiucAaBFO4XXYYUU7JGN_00',
+      'https://47.img.avito.st/image/1/1.viQF77ayEs0zRtDIdYW_NNlMEsulShA.NN3wFwEYiwzqxtU50FnRqf04k08Rjt7vjjdSsvvV2_k',
+      'https://25.img.avito.st/image/1/1.RMV7kLay6CxNOSopf-lF1acz6CrbNeo.Px_mi3Gh5ExEE77kkl1vmF4favz45TWoGPFNo2K-y8M',
+      'https://38.img.avito.st/image/1/1.s4Iv9bayH2sZXN1uRYuykvNWH22PUB0.KAPCIhzuWUbtuehXgKhaLfo9IeYCrNJbtE5MJqcRGFU',
+      'https://09.img.avito.st/image/1/1.xNigbrayaDGWx6o0pC3FyHzNaDcAy2o.zaZsngGwEN2XMys-dnoWDmpudfvGaGlQM0N2gG8nx1g',
+      'https://41.img.avito.st/image/1/1.QIdlkray7G5TOy5radNBl7kx7GjFN-4.SSOp8St6YB1jm9G13cx4BV42wz5AhZpA99TqIyqP1QE',
+      'https://13.img.avito.st/image/1/1.cDiHj7ay3NGxJh7U68pxKFss3NcnKt4.Q6t0hWjC7i8aGHabn3WMid_Cp5L7YdVVJntWycuIOVs',
+      'https://55.img.avito.st/image/1/1.5lj6ZLaySrHMzYi0ui_nSCbHSrdawUg.7Mr17P0o1EFMEOZZOIrWoVbvtRXM_lm9GnjARqACMe0',
+      'https://19.img.avito.st/image/1/1.st96uLayHjZMEdwzEuqzz6YbHjDaHRw.W9JrLNTOMzPXptqdxyAicqqgjgjSmF95Q4j0gZ7z4b4',
+      'https://19.img.avito.st/image/1/1.DioF77ayosMzRmDGFb8POtlMosWlSqA.TofH-o7ZALMbCu0jXGe2uSB0ozEFEuLUxDUCTKehw-k',
+      'https://38.img.avito.st/image/1/1.q4Iu1bayB2sYfMVudI-qkvJ2B22OcAU.zPwm0HptSaiIbRgIwhRw5p_JGo9yCM0ck33Z1z5IJqs',
+    ],
+    type: 'home',
+    link: 'https://www.avito.ru/chernoistochinsk/doma_dachi_kottedzhi/kottedzh_75m_na_uchastke_14sot._2075951755',
+    address: 'г.о. Нижний Тагил, пос. Уралец, Горнолыжная 29А (кажется)',
+    price: 60000,
+    options: {
+      'Гора Белая': 'шаговая доступность',
+      'Заезд': '30 декабря',
+      'Выезд': '2 января',
+      'Минусы': 'Маленький и всего на 7 человек'
     }
   },
 ]
